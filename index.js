89e3b8fe65156009a1f925ccc7c2405f13708a63
fen <jonairl@icloud.com>
2016-08-18 21:02:16 +0200

    poop more

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ module.exports = function (string) {
   var poop = emoji.get('hankey')
   return poop + ' ' + string + ' ' + poop
 }
+
+module.exports = function pooptimes (string, times) {
+  var string = string.trim()
+  var poop = 'aaa'
+  if (times === 0)
+    return string
+  return pooptimes(poop + ' ' + string + ' ' + poop, times - 1)
+}
